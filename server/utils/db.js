c5db59b9820ca7973266c1271ff71a0c2d757905
harshit964 <gargharshit040@gmail.com>
2025-07-01 00:47:46 +0530

    Add complete project source code

---
+++ server/utils/db.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+const connectDB = async() => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('mongodb connected successfully');
+    } catch (error) {
+        console.log(error);
+        console.log("mongo db conntion error")
+    }
+}
+export default connectDB;
